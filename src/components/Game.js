ff5f26254258770e680e449c9d03533fe866fcab
William Matias <wmatias@combustiongroup.com>
2017-11-28 16:08:00 -0500

    initial commit

---
+++ src/components/Game.js
@@ -0,0 +1,18 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react'
+import View from 'react-native'
+
+export default class App extends Component<{}> {
+    render() {
+        return (
+           <View style={}>
+               <Text>42</Text>
+           </View>
+        );
+    }
+}
